eb8f1daed3decde8aaa0b34202d73a58a06c4e33
zaiditime <zaiditime@localhost.localdomain>
2017-01-10 15:09:22 -0500

    fixing ip

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ if __name__ == '__main__':
 
     print "Starting app on port %d" % port
 
-    app.run(debug=False, port=port, host='127.0.0.1')
+    app.run(debug=False, port=port, host='0.0.0.0')
